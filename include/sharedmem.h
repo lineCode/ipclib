4b895bcfa6eac19670d7df5588aba42e5a7075ea
Slashcash <gen.iorio92@gmail.com>
2017-09-05 21:03:57 +0200

    Solve the sharedmem initialization bug

--- include/sharedmem.h
+++ include/sharedmem.h
@@ -30,11 +30,13 @@ namespace ipclib {
             SharedMem(const std::string& theName, const bool toCreate = true, const Protection theProtection = READ_AND_WRITE);
 
             Protection get_protection() const { return protection; }
-            T& get_value() const { return *obj_addr; }
+            T get_value() const { return *obj_addr; }
 
             void set_value(const T& theValue) { *obj_addr = theValue; }
 
             Result destroy();
+
+            operator T() { return get_value(); }
 
             ~SharedMem() { munmap(obj_addr, sizeof(T)); }
     };
@@ -45,6 +47,7 @@ namespace ipclib {
 
     template<class T> Result SharedMem<T>::create(const std::string& theName, const bool toCreate, const Protection theProtection) {
         protection = theProtection;
+        obj_addr = nullptr;
 
         int oflag;
         if( theProtection == READ_ONLY ) oflag = O_RDONLY;
@@ -53,7 +56,7 @@ namespace ipclib {
         if( toCreate ) oflag = oflag | O_CREAT;
 
         int fd;
-        if( (fd = shm_open(theName.c_str(), oflag, DEFAULT_PERMISSION)) != 0 ) return Result(errno, strerror(errno));
+        if( (fd = shm_open(theName.c_str(), oflag, DEFAULT_PERMISSION)) == -1 ) return Result(errno, strerror(errno));
 
         if( (ftruncate(fd, sizeof(T))) != 0 ) return Result(errno, strerror(errno));
 
@@ -62,9 +65,11 @@ namespace ipclib {
         else if( theProtection == WRITE_ONLY ) prot_oflag = PROT_WRITE;
         else prot_oflag = PROT_READ | PROT_WRITE;
 
-        if( (obj_addr = (T*)(mmap(NULL, sizeof(T), prot_oflag, MAP_SHARED, fd, NULL))) == (void*)(-1) ) return Result(errno, strerror(errno));
+        if( (obj_addr = (T*)(mmap(NULL, sizeof(T), prot_oflag, MAP_SHARED, fd, 0))) == MAP_FAILED ) return Result(errno, strerror(errno));
 
         if( close(fd) != 0 ) return Result(errno, strerror(errno));
+
+
 
         return Result(Result::SUCCESS);
     }
